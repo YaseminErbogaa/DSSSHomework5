c7cd2e3d61c35fcc94247c2719027056f7678df2
YaseminErbogaa <yaseminerbogaa@gmail.com>
2022-11-29 22:58:19 +0100

    Set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='snowflake',
+    version='0.1',
+    author='Yasemin',
+    author_email='yasemin.erboga@fau.edu',
+    packages=find_packages(),
+    install_require=['numpy', 'turtles']
+
+)
